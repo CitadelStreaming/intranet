4fe8ac47ad4558ce5eb388748a515f00735c4de7
James Blades <jwkblades+git@gmail.com>
2021-10-16 17:32:04 -0600

    Add main application, finally.

---
+++ src/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"citadel_intranet/src/application"
+	"citadel_intranet/src/config"
+	"citadel_intranet/src/db"
+	"citadel_intranet/src/server"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+	dbClient := db.NewDatabaseClient(cfg)
+
+	dbClient.Migrate(cfg.MigrationsPath)
+
+	webServer := server.NewServer(cfg)
+	app := application.NewApp(dbClient, webServer)
+	defer app.Close()
+	app.Run()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	s := <-c
+	fmt.Printf("Received signal, shutting down: %s", s)
+	app.Close()
+}
