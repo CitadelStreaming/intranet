5421b26771808230d9cea5e4ba596955b717f1ba
James Blades <jwkblades+git@gmail.com>
2021-10-16 22:34:54 -0600

    Need tracks to have album id passed around.

--- src/db/model/track.go
+++ src/db/model/track.go
@@ -3,6 +3,6 @@ package model
 type Track struct {
 	Id      int64  `json:"id"`
 	Title   string `json:"title"`
-	AlbumId int64  `json:"-"`
+	AlbumId int64  `json:"album"`
 	Rating  uint   `json:"rating"`
 }
